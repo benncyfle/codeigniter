34fd1bf456c1ce6dc7507150e6c8b84e30c2d231
benncyfle <benn@bennpearson.co.uk>
2011-08-19 12:35:13 +0100

    made changes to blog_entry, hello_view and index

--- application/views/hello_view.php
+++ application/views/hello_view.php
@@ -3,12 +3,20 @@
 </head>
 <body>
 
-<h1>Welcome <?php echo $v1 ?></h1>
+<h1>Welcome <?php echo $v1; ?></h1>
 <p>Codeigniter</p>
 
 <ul>
 	<?php foreach($row as $user): ?>
 		<li><?php echo $user; ?></li>
+	<?php endforeach; ?>
+</ul>
+
+<p>Blog</p>
+
+<ul>
+	<?php foreach($blog as $entry): ?>
+		<li><a href="/hello/blog/<?php echo $entry->slug; ?>"><?php echo $entry->slug; ?></a></li>
 	<?php endforeach; ?>
 </ul>
 
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors' , true);
+
+
 /*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
